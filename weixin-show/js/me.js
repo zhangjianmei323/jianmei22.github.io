02bc296f51173c6f574855ec202c51bbc944643f
jianmei22 <48115349+jianmei22@users.noreply.github.com>
2019-07-11 23:01:23 +0800

    Add files via upload

---
+++ weixin-show/js/me.js
@@ -0,0 +1,26 @@
+window.onload =function () {
+	$('.avatar').on('click', function() {
+		window.location.href = "./source/me/avatar.html";
+	})
+	$('.code').on('click', function() {
+		window.location.href = "./source/me/er-code.html";
+	})
+	$('.pay').on('click', function() {
+		window.location.href = "./source/me/pay.html";
+	})
+	$('.collect').on('click', function() {
+		window.location.href = "./source/me/collection.html";
+	})
+	$('.ablum').on('click', function() {
+		window.location.href = "./source/me/ablum.html";
+	})
+	$('.card').on('click', function() {
+		window.location.href = "./source/me/card.html";
+	})
+	$('.look').on('click', function() {
+		window.location.href = "./source/me/look.html";
+	})
+	$('.set').on('click', function() {
+		window.location.href = "./source/me/set.html";
+	})
+}
